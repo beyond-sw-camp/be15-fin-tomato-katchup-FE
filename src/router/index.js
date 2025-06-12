f35f0522e9fd0af5458c0b38335b5295d4db8037
Cho-Hyun-Seung <toki0327@naver.com>
2025-06-12 15:02:28 +0900

    chore: 라우터 생성

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
 import { mainRoutes } from '@/features/main/router.js'
+import { advertisementRoutes } from '@/features/advertisement/router'
+import { calendarRoutes } from '@/features/calendar/router'
+import { campaignRoutes } from '@/features/campaign/router'
+import { chatRoutes } from '@/features/chat/router'
+import { contractRoutes } from '@/features/contract/router'
+import { dashboardRoutes } from '@/features/dashboard/router'
+import { userRoutes } from '@/features/user/router'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     ...mainRoutes,
+    ...advertisementRoutes,
+    ...calendarRoutes,
+    ...campaignRoutes,
+    ...chatRoutes,
+    ...contractRoutes,
+    ...dashboardRoutes,
+    ...userRoutes,
     // 아래로 쭉 추가해주세요
   ],
   scrollBehavior() {
-    return { left: 0, top: 0 };
+    return { left: 0, top: 0 }
   },
-});
+})
 
-export default router;
+export default router
