2307359688bf9578ff9ab7cd9ff525f7bdfdf411
Cho-Hyun-Seung <toki0327@naver.com>
2025-06-12 22:43:08 +0900

    chore: tailwind, prettier 설정 수정

--- src/main.js
+++ src/main.js
@@ -1,29 +1,27 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
-import Toast from 'vue-toastification'
-import 'vue-toastification/dist/index.css'
-import './assets/styles/tailwind.css'
+import App from './App.vue';
+import router from './router';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+import './assets/styles/tailwind.css';
 
 async function bootstrap() {
-  const app = createApp(App)
+    const app = createApp(App);
 
-  app.use(createPinia())
-  app.use(router)
-  app.use(Toast)
+    app.use(createPinia());
+    app.use(router);
+    app.use(Toast);
 
-  // if(import.meta.env.NODE_ENV === 'deployment'){
-    console.log("!!!!!");
-    const { worker } = await import('@/mocks/api/browser.js')
-    await worker.start()
+    if (import.meta.env.DEV) {
+        const { worker } = await import('@/mocks/api/browser.js');
+        await worker.start();
+    }
 
-  // }
-
-  app.mount('#app')
+    app.mount('#app');
 }
 
-;(async () => {
-  await bootstrap()
-})()
+(async () => {
+    await bootstrap();
+})();
