69015e1da2c9eda2012b423d65dbe240acb9456d
oyk0510 <youkyungoh0510@naver.com>
2025-06-13 17:31:06 +0900

    feat: 사이드바, 페이지네이션, 카드 리스트 추가 및 삭제

--- src/features/advertisement/views/ClientListView.vue
+++ src/features/advertisement/views/ClientListView.vue
@@ -1,27 +1,32 @@
 <template>
-  <div class="flex flex-col w-full px-8 pt-6">
-    <!-- 상단 텍스트 -->
-    <div class="mb-4 text-lg font-bold text-black">
-      고객사 목록
-      <span class="text-sm font-normal text-gray-medium">(검색 결과: {{ totalCount }}건)</span>
-    </div>
+  <div class="flex w-full px-8 pt-6 gap-8">
+    <!-- 사이드바 -->
+    <CommonFiltering />
 
-    <!-- 카드 리스트 -->
-    <div class="grid grid-cols-2 gap-6">
-      <ClientCard
-        v-for="client in paginatedClients"
-        :key="client.id"
-        :client="client"
-      />
-    </div>
+    <!-- 본문 영역 -->
+    <div class="flex flex-col flex-1 container bg-white">
+      <div class="mt-6 mb-4 text-lg font-bold text-black ml-4">
+        고객사 목록
+        <span class="text-sm font-normal text-gray-medium">(검색 결과: {{ totalCount }}건)</span>
+      </div>
 
-    <!-- 페이지네이션 -->
-    <div class="flex justify-center mt-8">
-      <PagingBar
-        :totalPages="totalPages"
-        :currentPage="currentPage"
-        @update:currentPage="(val) => currentPage = val"
-      />
+      <!-- 카드 리스트 -->
+      <div class="grid grid-cols-2 gap-x-6 gap-y-6 px-4">
+        <ClientCard
+          v-for="client in paginatedClients"
+          :key="client.id"
+          :client="client"
+        />
+      </div>
+
+      <!-- 페이지네이션 -->
+      <div class="flex justify-center mt-8">
+        <PagingBar
+          :totalPages="totalPages"
+          :currentPage="currentPage"
+          @update:currentPage="(val) => currentPage = val"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -30,13 +35,14 @@
 import { ref, computed } from 'vue'
 import ClientCard from '../components/ClientCard.vue'
 import PagingBar from '@/components/common/PagingBar.vue'
+import CommonFiltering from '@/components/layout/CommonFiltering.vue'
 
 const currentPage = ref(1)
 const pageSize = 6
 
 const clientList = ref([
   { id: 1, name: 'ABC 화장품', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
-  { id: 2, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경', '이승재'], price: '23,370,000' },
+  { id: 2, name: '아모레퍼시픽', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경', '이승재'], price: '23,370,000' },
   { id: 3, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
   { id: 4, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
   { id: 5, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
