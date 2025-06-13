b90030db48e781237a5f6b3599abf7f93a388267
lddocy <jkcy911219@naver.com>
2025-06-13 15:03:10 +0900

    fix: 필터링 사이드바 수정

--- src/components/layout/CommonFiltering.vue
+++ src/components/layout/CommonFiltering.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="sticky top-10 w-[304px] h-[630px] bg-white rounded-2xl shadow-md p-6">
+  <div class="sticky top-10 mt-10 w-[304px] h-[630px] bg-white rounded-2xl shadow-md p-6">
     <p class="text text-md font-semibold mb-4 mt-5">검색조건</p>
 
-    <select class="appearance-none leading-tight focus:outline-none w-full border border-gray-medium rounded-md p-2 mb-3">
-      <option>전체</option>
-      <option>캠페인명</option>
-      <option>광고업체명</option>
-      <option>광고담당자명</option>
-    </select>
+    <div class="relative">
+      <select class="appearance-none leading-tight focus:outline-none w-full border border-gray-medium rounded-md p-2 mb-3">
+        <option>전체</option>
+        <option>캠페인명</option>
+        <option>광고업체명</option>
+        <option>광고담당자명</option>
+      </select>
+
+      <div class="absolute inset-y-0 mb-2 right-3 flex items-center pointer-events-none">
+        <i class="bi bi-chevron-down text-black"></i>
+      </div>
+    </div>
 
     <input type="text" placeholder="검색어" class="w-full border border-gray-medium rounded-md p-2 mb-3" />
 
@@ -22,18 +28,18 @@
 
     <p class="text text-md font-semibold mb-4 mt-5">필터/정렬</p>
 
-    <select class="appearance-none leading-tight focus:outline-none w-full border border-gray-medium rounded-md p-2 mb-3">
-      <option>필터</option>
-      <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-        ▼
+    <div class="relative">
+      <select class="appearance-none leading-tight focus:outline-none w-full border border-gray-medium rounded-md p-2 mb-3">
+        <option>필터</option>
+      </select>
+      <div class="absolute inset-y-0 mb-2 right-3 flex items-center pointer-events-none">
+        <i class="bi bi-chevron-down text-black"></i>
       </div>
-    </select>
+    </div>
 
     <div class="flex gap-2 mb-3">
-      <select class="flex-1 border border-gray-medium rounded-md p-2">
-        <option>등록일</option>
-      </select>
-      <button class="w-[40px] h-[40px] bg-btn-gray border border-gray-medium rounded-md flex items-center justify-center">
+      <input type="date" class="flex-1 border border-gray-medium rounded-md p-2 leading-tight focus:outline-none" />
+      <button class="w-[40px] h-[40px] bg-btn-gray rounded-md flex items-center justify-center">
         <img src="@/assets/icons/sort.svg" alt="sort" class="w-5 select-none" />
       </button>
     </div>
