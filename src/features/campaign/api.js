6044a9aee9b205dff8ef6198a5bf1c9c240be412
Cho-Hyun-Seung <toki0327@naver.com>
2025-06-16 15:31:16 +0900

    feat: 제안 목록 페이지 개발

--- src/features/campaign/api.js
+++ src/features/campaign/api.js
@@ -18,29 +18,64 @@ export const getCampaignHistory = async (campaignId) => {
     return await api.get(`/campaign/history/${campaignId}`);
 };
 
-export const getQuotationDetail = async (quotationId) => {
-    return await api.get(`/quotation/${quotationId}`);
-};
-
 export const getOpinion = async (targetId, targetType) => {
     return await api.get('/opinion', {
-        params: {
-            targetType,
-            targetId,
-        },
+        params: { targetType, targetId },
     });
 };
 
-export const getProposalList = async (page, size) => {
+export const getProposalList = async (page, size, filters = {}) => {
     return await api.get('/sales/proposal', {
-        params: {
-            page,
-            size,
-        },
+        params: { page, size, ...filters },
     });
 };
 
-// === 팝업용 API ===
+export const getProposalDetail = async (proposalId) => {
+    return await api.get(`/sales/proposal/${proposalId}`);
+};
+
+export const getListUpList = async (page, size, filters = {}) => {
+    return await api.get('/sales/list-up', {
+        params: { page, size, ...filters },
+    });
+};
+
+export const getListUpDetail = async (listUpId) => {
+    return await api.get(`/sales/list-up/${listUpId}`);
+};
+
+export const getQuotationList = async (page, size, filters = {}) => {
+    return await api.get('/sales/quotation', {
+        params: { page, size, ...filters },
+    });
+};
+
+export const getQuotationDetail = async (quotationId) => {
+    return await api.get(`/sales/quotation/${quotationId}`);
+};
+
+export const getSalesList = async (page, size, filters = {}) => {
+    return await api.get('/sales/sale', {
+        params: { page, size, ...filters },
+    });
+};
+
+export const getSalesDetail = async (salesId) => {
+    return await api.get(`/sales/sale/${salesId}`);
+};
+
+export const getContractList = async (page, size, filters = {}) => {
+    return await api.get('/sales/contract', {
+        params: { page, size, ...filters },
+    });
+};
+
+export const getContractDetail = async (contractId) => {
+    return await api.get(`/sales/contract/${contractId}`);
+};
+
+/* === 팝업 ===*/
+
 export const getUser = async () => {
     return await api.get('/popup/user');
 };
@@ -53,11 +88,10 @@ export const getClientManager = async () => {
     return await api.get('/popup/client-manager');
 };
 
-export const getProposalReference = async () => {
-    return await api.get('/proposal/reference');
-};
-
 export const getInfluencer = async () => {
     return await api.get('/popup/influencer');
 };
-// ======
+
+export const getProposalReference = async () => {
+    return await api.get('/proposal/reference');
+};
--- src/mocks/api/handler/CampaignHandler.js
+++ src/mocks/api/handler/CampaignHandler.js
@@ -246,6 +246,30 @@ const proposalList = Array.from({ length: 50 }, (_, index) => {
     };
 });
 
+const quotationList = Array.from({ length: 50 }, (_, index) => {
+    const baseDate = new Date();
+    baseDate.setFullYear(baseDate.getFullYear() + index);
+
+    const statusArr = ['승인완료', '승인요청', '보류/대기', '승인거절'];
+
+    return {
+        id: index + 1,
+        status: statusArr[Math.floor(Math.random() * 4)],
+        title: `Quotation ${index + 1}`,
+        createdDate: formatDate(new Date(baseDate.getFullYear() + 2, 2, 20)),
+        campaignTitle: `XYZ 패션 브랜드 시즌 프로모션 계약`,
+        clientCompany: `Company ${index + 1}`,
+        clientManagerName: '김동영',
+        clientManagerPosition: '팀장',
+        userName: '정재현',
+        userPosition: '과장',
+        productName: '조던 브루클린 플리스 풀오버 후디',
+        adPrice: 30000000,
+        requestDate: formatDate(new Date(baseDate.getFullYear() + 2, 2, 20)),
+        presentDate: formatDate(new Date(baseDate.getFullYear() + 2, 2, 20)),
+    };
+});
+
 const CampaignHandler = [
     http.get('/api/v1/campaign', async ({ request }) => {
         const url = new URL(request.url);
@@ -300,7 +324,7 @@ const CampaignHandler = [
             },
         );
     }),
-    http.get('/api/v1/quotation/:id', async () => {
+    http.get('/api/v1/sales/quotation/:id', async () => {
         return HttpResponse.json(
             {
                 data: quotationDetail,
@@ -358,6 +382,28 @@ const CampaignHandler = [
                 page,
                 size,
                 total: proposalList.length,
+            },
+            {
+                status: 200,
+                headers: { 'Cache-Control': 'no-store' },
+            },
+        );
+    }),
+    http.get('/api/v1/sales/quotation', async ({ request }) => {
+        const url = new URL(request.url);
+        const page = parseInt(url.searchParams.get('page')) || 1;
+        const size = parseInt(url.searchParams.get('size')) || 10;
+
+        const startIndex = (page - 1) * size;
+        const endIndex = startIndex + size;
+        const pagedData = quotationList.slice(startIndex, endIndex);
+
+        return HttpResponse.json(
+            {
+                data: pagedData,
+                page,
+                size,
+                total: quotationList.length,
             },
             {
                 status: 200,
