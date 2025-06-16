b3b634a735f801eb20b6e36948cd50ba96d5fe48
oyk0510 <youkyungoh0510@naver.com>
2025-06-16 09:59:29 +0900

    style: 구성 수정

--- src/features/advertisement/views/ClientListView.vue
+++ src/features/advertisement/views/ClientListView.vue
@@ -1,3 +1,32 @@
+<script setup>
+import { ref, computed } from 'vue'
+import ClientCard from '../components/ClientCard.vue'
+import PagingBar from '@/components/common/PagingBar.vue'
+import CommonFiltering from '@/components/layout/CommonFiltering.vue'
+
+const currentPage = ref(1)
+const pageSize = 6
+
+const clientList = ref([
+  { id: 1, name: 'ABC 화장품', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
+  { id: 2, name: '아모레퍼시픽', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경', '이승재'], price: '23,370,000' },
+  { id: 3, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
+  { id: 4, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
+  { id: 5, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
+  { id: 6, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
+  { id: 7, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
+  { id: 8, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
+])
+
+const totalCount = clientList.value.length
+const totalPages = computed(() => Math.ceil(totalCount / pageSize))
+
+const paginatedClients = computed(() => {
+  const start = (currentPage.value - 1) * pageSize
+  return clientList.value.slice(start, start + pageSize)
+})
+</script>
+
 <template>
   <div class="w-full min-h-screen bg-background flex font-sans">
     <!-- 사이드바 -->
@@ -42,32 +71,3 @@
     </div>
   </div>
 </template>
-
-<script setup>
-import { ref, computed } from 'vue'
-import ClientCard from '../components/ClientCard.vue'
-import PagingBar from '@/components/common/PagingBar.vue'
-import CommonFiltering from '@/components/layout/CommonFiltering.vue'
-
-const currentPage = ref(1)
-const pageSize = 6
-
-const clientList = ref([
-  { id: 1, name: 'ABC 화장품', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
-  { id: 2, name: '아모레퍼시픽', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경', '이승재'], price: '23,370,000' },
-  { id: 3, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
-  { id: 4, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
-  { id: 5, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
-  { id: 6, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
-  { id: 7, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
-  { id: 8, name: '마니커', address: '서울특별시 구로구 디지털로 76길 111(구로동)', contacts: ['오유경'], price: '23,370,000' },
-])
-
-const totalCount = clientList.value.length
-const totalPages = computed(() => Math.ceil(totalCount / pageSize))
-
-const paginatedClients = computed(() => {
-  const start = (currentPage.value - 1) * pageSize
-  return clientList.value.slice(start, start + pageSize)
-})
-</script>
