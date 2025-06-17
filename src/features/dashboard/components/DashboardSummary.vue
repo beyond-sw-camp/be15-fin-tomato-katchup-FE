65e00688dec4b0f8af0da241bf2b056603b3c4fe
lddocy <jkcy911219@naver.com>
2025-06-17 16:01:27 +0900

    fix: 유튜브/인스타 대시보드 구분선 추가

--- src/features/dashboard/components/DashboardSummary.vue
+++ src/features/dashboard/components/DashboardSummary.vue
@@ -17,13 +17,22 @@ const getTitle = () => {
 }
 </script>
 
+
 <template>
   <div class="dashboard-section">
-    <div class="grid grid-cols-4 gap-10 text-center">
-      <SummaryItem :title="getTitle()" :value="data.shorts" icon="ph:play-circle-bold" />
-      <SummaryItem title="평균 조회 수" :value="data.views" icon="mdi:eye-outline" />
-      <SummaryItem title="평균 댓글 수" :value="data.comments" icon="mdi:comment-outline" />
-      <SummaryItem title="평균 좋아요 수" :value="data.likes" icon="mdi:heart-outline" />
+    <div class="grid grid-cols-4 text-center divide-x divide-gray-medium">
+      <div class="px-4">
+        <SummaryItem :title="getTitle()" :value="data.shorts" icon="ph:play-circle-bold" />
+      </div>
+      <div class="px-4">
+        <SummaryItem title="평균 조회 수" :value="data.views" icon="mdi:eye-outline" />
+      </div>
+      <div class="px-4">
+        <SummaryItem title="평균 댓글 수" :value="data.comments" icon="mdi:comment-outline" />
+      </div>
+      <div class="px-4">
+        <SummaryItem title="평균 좋아요 수" :value="data.likes" icon="mdi:heart-outline" />
+      </div>
     </div>
   </div>
 </template>
